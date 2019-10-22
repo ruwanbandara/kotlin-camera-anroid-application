74997fa5d2100f17bd877e882149a526591573fe
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-22 14:43:40 +0530

    update

--- app/src/main/java/ruwanbandara/camera/MainActivity.kt
+++ app/src/main/java/ruwanbandara/camera/MainActivity.kt
@@ -1,12 +1,28 @@
 package ruwanbandara.camera
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //button click
+        capture_btn.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission) == packageManager.PERMISSION_DENIED)
+
+            }
+
+        }
+
+
     }
 }
